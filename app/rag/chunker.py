b8d2ced2e0555762e1c088c1d2d4d32723885d90
Aryan Prasad <aryanprasad2218@gmail.com>
2026-04-22 03:48:13 +0530

    Fixed LangChain import issue

--- app/rag/chunker.py
+++ app/rag/chunker.py
@@ -1,6 +1,5 @@
 # app/rag/chunker.py
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 def chunk_text(text):
 
